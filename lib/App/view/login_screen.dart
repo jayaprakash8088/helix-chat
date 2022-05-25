6ae4e3218e783d7648692fd51f3f5a76bc239cfd
Jayaprakash S <70194564+jayaprakash8088@users.noreply.github.com>
2022-05-25 19:53:49 +0530

    working on chat login splash

---
+++ lib/App/view/login_screen.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/App/view/splash_screen.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
